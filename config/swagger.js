30d682b746ffd245429cddb6ccdf86b85efe4ea4
jay <jay.huerte@neu.edu.ph>
2025-12-05 09:36:28 +0800

    Vercel Swagger UI

--- config/swagger.js
+++ config/swagger.js
@@ -4,22 +4,23 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Your API Documentation",
+      title: "Fitness Tracker API",
       version: "1.0.0",
-      description: "API documentation for your project",
+      description: "API documentation for fitness tracker",
     },
     servers: [
-  {
-    url: "http://localhost:3000",
-    description: "Development server"
+      {
+        url: "http://localhost:3000",
+        description: "Development server"
+      },
+      {
+        url: process.env.VERCEL_URL 
+          ? `https://${process.env.VERCEL_URL}`
+          : "https://fitness-tracker-api-orcin.vercel.app",
+        description: "Production server"
+      },
+    ],
   },
-  {
-    url: "https://fitness-tracker-api-orcin.vercel.app/",
-    description: "Production server"
-  },
-],
-  },
-
   apis: ["./routes/*.js"],
 };
 
